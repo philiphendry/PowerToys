4698f0bcaaddc294aee7e70ebea74a997fb49ba9
Philip Hendry <philiphmhendry@gmail.com>
2024-11-16 12:27:29 +0000

    Refactored code and added debug logging

--- src/modules/QuickWindows/QuickWindowsUI/QuickWindowsManager.cs
+++ src/modules/QuickWindows/QuickWindowsUI/QuickWindowsManager.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.Composition;
 using System.Threading;
 using System.Windows;
 using Common.UI;
+using ManagedCommon;
 using PowerToys.Interop;
 using QuickWindows.Keyboard;
 using QuickWindows.Mouse;
@@ -18,6 +19,7 @@ namespace QuickWindows;
 [PartCreationPolicy(CreationPolicy.Shared)]
 public class QuickWindowsManager : IQuickWindowsManager
 {
+    private static readonly object Lock = new object();
     private readonly IUserSettings _userSettings;
     private readonly IKeyboardMonitor _keyboardHook;
     private readonly IMouseHook _mouseHook;
@@ -35,6 +37,9 @@ public class QuickWindowsManager : IQuickWindowsManager
         _keyboardHook = keyboardHook;
         _mouseHook = mouseHook;
 
+        AddKeyboardListeners();
+        AddMouseListeners();
+
         NativeEventWaiter.WaitForEventLoop(
             Constants.TerminateQuickWindowsSharedEvent(),
             Application.Current.Shutdown,
@@ -50,79 +55,9 @@ public class QuickWindowsManager : IQuickWindowsManager
 
     public void ActivateHotKey()
     {
-        _keyboardHook.AltKeyPressed += (_, _) =>
-        {
-            _isAltPressed = true;
-            _mouseHook.Install();
-            _mouseHook.SuppressLeftClick = true;
-        };
-
-        _keyboardHook.AltKeyReleased += (_, _) =>
-        {
-            _isAltPressed = false;
-            WindowOperations.EndWindowDrag();
-            _mouseHook.SuppressLeftClick = false;
-            _mouseHook.Uninstall();
-        };
-
-        _mouseHook.MouseDown += (_, args) =>
-        {
-            if (!_isAltPressed)
-            {
-                return;
-            }
-
-            _isMoveOperation = args.Button == MouseButton.Left;
-            WindowOperations.StartWindowOperation(args.X, args.Y, _isMoveOperation);
-        };
-
-        _mouseHook.MouseMove += (_, args) =>
-        {
-            if (!_isAltPressed)
-            {
-                return;
-            }
-
-            if (_isMoveOperation)
-            {
-                WindowOperations.MoveWindowWithMouse(args.X, args.Y);
-            }
-            else
-            {
-                WindowOperations.ResizeWindowWithMouse(args.X, args.Y);
-            }
-        };
-
-        _mouseHook.MouseUp += (_, args) =>
-        {
-            if (args.Button != MouseButton.Left && args.Button != MouseButton.Right)
-            {
-                return;
-            }
-
-            WindowOperations.EndWindowDrag();
-        };
-
-        _mouseHook.MouseWheel += (_, args) =>
-        {
-            if (!_isAltPressed)
-            {
-                return;
-            }
-
-            // Positive delta means wheel up, negative means wheel down
-            if (args.Delta > 0)
-            {
-                WindowOperations.SendWindowToBottom(args.X, args.Y);
-            }
-            else
-            {
-                WindowOperations.BringBottomWindowToTop(args.X, args.Y);
-            }
-        };
-
         try
         {
+            Logger.LogDebug("Installing keyboard hook.");
             _keyboardHook.Install();
         }
         catch (Exception ex)
@@ -133,7 +68,120 @@ public class QuickWindowsManager : IQuickWindowsManager
 
     public void DeactivateHotKey()
     {
+        Logger.LogDebug("Uninstalling mouse and keyboard hooks.");
         _mouseHook.Uninstall();
         _keyboardHook.Uninstall();
+    }
+
+    private void AddKeyboardListeners()
+    {
+        _keyboardHook.AltKeyPressed += OnAltKeyPressed;
+        _keyboardHook.AltKeyReleased += OnAltKeyReleased;
+    }
+
+    private void AddMouseListeners()
+    {
+        _mouseHook.MouseDown += OnMouseDown;
+        _mouseHook.MouseMove += OnMouseMove;
+        _mouseHook.MouseUp += OnMouseUp;
+        _mouseHook.MouseWheel += OnMouseWheel;
+    }
+
+    private void OnAltKeyPressed(object? o, KeyboardMonitor.KeyPressedEventArgs keyPressedEventArgs)
+    {
+        lock (Lock)
+        {
+            if (_isAltPressed)
+            {
+                return;
+            }
+
+            Logger.LogDebug("Installing mouse hook.");
+            _isAltPressed = true;
+            _mouseHook.Install();
+            _mouseHook.SuppressLeftClick = true;
+        }
+    }
+
+    private void OnAltKeyReleased(object? o, KeyboardMonitor.KeyPressedEventArgs keyPressedEventArgs)
+    {
+        lock (Lock)
+        {
+            if (!_isAltPressed)
+            {
+                return;
+            }
+
+            Logger.LogDebug("EndWindowDrag and uninstall mouse hook.");
+            _isAltPressed = false;
+            WindowOperations.EndWindowDrag();
+            _mouseHook.SuppressLeftClick = false;
+            _mouseHook.Uninstall();
+        }
+    }
+
+    private void OnMouseDown(object? target, MouseHook.MouseEventArgs args)
+    {
+        lock (Lock)
+        {
+            if (!_isAltPressed)
+            {
+                return;
+            }
+
+            _isMoveOperation = args.Button == MouseButton.Left;
+            Logger.LogDebug($"StartWindowOperation _isMoveOperation: {_isMoveOperation}");
+            WindowOperations.StartWindowOperation(args.X, args.Y, _isMoveOperation);
+        }
+    }
+
+    private void OnMouseUp(object? target, MouseHook.MouseEventArgs args)
+    {
+        lock (Lock)
+        {
+            if (!_isAltPressed || (args.Button != MouseButton.Left && args.Button != MouseButton.Right))
+            {
+                return;
+            }
+
+            Logger.LogDebug("EndWindowDrag");
+            WindowOperations.EndWindowDrag();
+        }
+    }
+
+    private void OnMouseMove(object? target, MouseHook.MouseEventArgs args)
+    {
+        if (!_isAltPressed && args.Button != MouseButton.Left && args.Button != MouseButton.Right)
+        {
+            return;
+        }
+
+        Logger.LogDebug($"_isMoveOperation: {_isMoveOperation}");
+        if (_isMoveOperation)
+        {
+            WindowOperations.MoveWindowWithMouse(args.X, args.Y);
+        }
+        else
+        {
+            WindowOperations.ResizeWindowWithMouse(args.X, args.Y);
+        }
+    }
+
+    private void OnMouseWheel(object? target, MouseHook.MouseWheelEventArgs args)
+    {
+        if (!_isAltPressed)
+        {
+            return;
+        }
+
+        // Positive delta means wheel up, negative means wheel down
+        if (args.Delta > 0)
+        {
+            WindowOperations.SendWindowToBottom(args.X, args.Y);
+        }
+        else
+        {
+            WindowOperations.BringBottomWindowToTop(args.X, args.Y);
+        }
     }
 }
