bc8932db221eec3f38b5084272557785db49d03f
Philip Hendry <philiphmhendry@gmail.com>
2025-01-12 08:50:28 +0000

    Refactor GlobalKeyboardHook to use own hinstance rather than loading user32.dll

--- src/modules/QuickWindows/QuickWindowsUI/Keyboard/GlobalKeyboardHook.cs
+++ src/modules/QuickWindows/QuickWindowsUI/Keyboard/GlobalKeyboardHook.cs
@@ -12,23 +12,14 @@ namespace QuickWindows.Keyboard
     public class GlobalKeyboardHook : IDisposable
     {
         private IntPtr _windowsHookHandle;
-        private IntPtr _user32LibraryHandle;
         private NativeMethods.HookProc? _hookProc;
 
         public GlobalKeyboardHook()
         {
             _windowsHookHandle = IntPtr.Zero;
-            _user32LibraryHandle = IntPtr.Zero;
             _hookProc = LowLevelKeyboardProc; // we must keep alive _hookProc, because GC is not aware about SetWindowsHookEx behaviour.
 
-            _user32LibraryHandle = NativeMethods.LoadLibrary("User32");
-            if (_user32LibraryHandle == IntPtr.Zero)
-            {
-                int errorCode = Marshal.GetLastWin32Error();
-                throw new Win32Exception(errorCode, $"Failed to load library 'User32.dll'. Error {errorCode}: {new Win32Exception(Marshal.GetLastWin32Error()).Message}.");
-            }
-
-            _windowsHookHandle = NativeMethods.SetWindowsHookEx(NativeMethods.WH_KEYBOARD_LL, _hookProc, _user32LibraryHandle, 0);
+            _windowsHookHandle = NativeMethods.SetWindowsHookEx(NativeMethods.WH_KEYBOARD_LL, _hookProc, Marshal.GetHINSTANCE(typeof(GlobalKeyboardHook).Module), 0);
             if (_windowsHookHandle == IntPtr.Zero)
             {
                 int errorCode = Marshal.GetLastWin32Error();
@@ -40,35 +31,26 @@ namespace QuickWindows.Keyboard
 
         protected virtual void Dispose(bool disposing)
         {
-            if (disposing)
+            if (!disposing)
             {
-                // because we can unhook only in the same thread, not in garbage collector thread
-                if (_windowsHookHandle != IntPtr.Zero)
-                {
-                    if (!NativeMethods.UnhookWindowsHookEx(_windowsHookHandle))
-                    {
-                        int errorCode = Marshal.GetLastWin32Error();
-                        throw new Win32Exception(errorCode, $"Failed to remove keyboard hooks for '{Process.GetCurrentProcess().ProcessName}'. Error {errorCode}: {new Win32Exception(Marshal.GetLastWin32Error()).Message}.");
-                    }
-
-                    _windowsHookHandle = IntPtr.Zero;
-
-                    // ReSharper disable once DelegateSubtraction
-                    _hookProc -= LowLevelKeyboardProc;
-                }
+                return;
             }
 
-            if (_user32LibraryHandle != IntPtr.Zero)
+            // because we can unhook only in the same thread, not in garbage collector thread
+            if (_windowsHookHandle == IntPtr.Zero)
             {
-                // reduces reference to library by 1.
-                if (!NativeMethods.FreeLibrary(_user32LibraryHandle))
-                {
-                    int errorCode = Marshal.GetLastWin32Error();
-                    throw new Win32Exception(errorCode, $"Failed to unload library 'User32.dll'. Error {errorCode}: {new Win32Exception(Marshal.GetLastWin32Error()).Message}.");
-                }
-
-                _user32LibraryHandle = IntPtr.Zero;
+                return;
             }
+
+            if (!NativeMethods.UnhookWindowsHookEx(_windowsHookHandle))
+            {
+                int errorCode = Marshal.GetLastWin32Error();
+                throw new Win32Exception(errorCode, $"Failed to remove keyboard hooks for '{Process.GetCurrentProcess().ProcessName}'. Error {errorCode}: {new Win32Exception(Marshal.GetLastWin32Error()).Message}.");
+            }
+
+            _windowsHookHandle = IntPtr.Zero;
+
+            _hookProc -= LowLevelKeyboardProc;
         }
 
         ~GlobalKeyboardHook()
@@ -92,23 +74,50 @@ namespace QuickWindows.Keyboard
 
         private IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam)
         {
-            bool fEatKeyStroke = false;
-
+            var fEatKeyStroke = false;
             var wparamTyped = wParam.ToInt32();
             if (Enum.IsDefined(typeof(KeyboardState), wparamTyped))
             {
-                object? o = Marshal.PtrToStructure(lParam, typeof(NativeMethods.LowLevelKeyboardInputEvent));
-                NativeMethods.LowLevelKeyboardInputEvent p = (NativeMethods.LowLevelKeyboardInputEvent)o!;
+                var o = Marshal.PtrToStructure(lParam, typeof(LowLevelKeyboardInputEvent));
+                if (o is not null)
+                {
+                    var eventArguments = new GlobalKeyboardHookEventArgs((LowLevelKeyboardInputEvent)o, (KeyboardState)wparamTyped);
+                    KeyboardPressed?.Invoke(this, eventArguments);
 
-                var eventArguments = new GlobalKeyboardHookEventArgs(p, (KeyboardState)wparamTyped);
-
-                EventHandler<GlobalKeyboardHookEventArgs>? handler = KeyboardPressed;
-                handler?.Invoke(this, eventArguments);
-
-                fEatKeyStroke = eventArguments.Handled;
+                    fEatKeyStroke = eventArguments.Handled;
+                }
             }
 
-            return fEatKeyStroke ? (IntPtr)1 : NativeMethods.CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+            return fEatKeyStroke ? 1 : NativeMethods.CallNextHookEx(IntPtr.Zero, nCode, wParam, lParam);
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct LowLevelKeyboardInputEvent
+        {
+            /// <summary>
+            /// A virtual-key code. The code must be a value in the range 1 to 254.
+            /// </summary>
+            public int VirtualCode;
+
+            /// <summary>
+            /// A hardware scan code for the key.
+            /// </summary>
+            public int HardwareScanCode;
+
+            /// <summary>
+            /// The extended-key flag, event-injected Flags, context code, and transition-state flag. This member is specified as follows. An application can use the following values to test the keystroke Flags. Testing LLKHF_INJECTED (bit 4) will tell you whether the event was injected. If it was, then testing LLKHF_LOWER_IL_INJECTED (bit 1) will tell you whether or not the event was injected from a process running at lower integrity level.
+            /// </summary>
+            public int Flags;
+
+            /// <summary>
+            /// The time stamp for this message, equivalent to what GetMessageTime would return for this message.
+            /// </summary>
+            public int TimeStamp;
+
+            /// <summary>
+            /// Additional information associated with the message.
+            /// </summary>
+            public IntPtr AdditionalInformation;
         }
     }
 }
